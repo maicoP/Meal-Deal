4ebb23c82e13c7760a937e824890784fadcc7c7e
unknown <maicopaulussen@hotmail.be>
2014-10-16 09:41:24 +0200

    database file aanpassen

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,7 +23,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'examen',
+			'database'  => 'mealdeal',
 			'username'  => 'homestead',
 			'password'  => 'secret',
 			'charset'   => 'utf8',
