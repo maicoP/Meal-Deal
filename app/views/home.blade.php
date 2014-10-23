136f45ee6949f548f2baa31a34e45a9f606a224f
Matthias Verhoeven <matthiasverhoeven@gmail.com>
2014-10-23 09:08:29 +0200

    Stijlen homepagina

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.default')
 
 @section('content')
-	<h1>Home</h1>
-
-	<div>
+	<div class="authencication">
 		{{ Form::open(['route' => 'sessions.store']) }}
+			<div class="login">
 			<h3>Log in</h3>
 			<div>
 				{{ $errors->first('email')}}
@@ -21,11 +20,16 @@
 				{{ Form::password('password')}}
 				
 			</div>
-			<div>
-				{{ Form::submit('Login')}}
-				{{link_to("users/create", "Register")}}
+			<div class="down">
+				{{ Form::submit('Log in', ['class' => 'button'])}}
 			</div>
-		{{ Form::close() }}
-		
+			{{ Form::close() }}
+			</div>
+			<div class="register">
+			<h3>Registreer</h3>
+			<p class="registersubtitle"><b>Je gratis account</b></p>
+			<p class="registertext">Begin binnen enkele minuten deals te claimen en deals te doneren</p>
+			<div class="down downplus">{{link_to("users/create", "Registreer", ['class' => 'button'])}}</div>
+			</div>
 	</div>
 @stop
